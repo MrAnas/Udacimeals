2a637df5b86943d472c7c81b12f468464ba6e6f3
Anas <anas2006101@gmail.com>
2018-05-16 11:03:47 +0300

    combineReducers

--- udacimeals/src/components/App.js
+++ udacimeals/src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
+import {addRecipe,removeFromCalender} from '../actions';
 
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -12,5 +15,24 @@ class App extends Component {
     );
   }
 }
+function mapsStateToProps({calander,food}) {
+  const dayOrder = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+  // Review later
+  return {
+    calander: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calander[day]).reduce((meals,meal)=>{
+        meals[meal] = calander[day][meal] ? food[calander[day][meal]] : null
+      },{})
+  }))
+}
+}
 
-export default App;
+function mapDispatchToProps(dispatch){
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalender(data))
+  }
+}
+
+export default connect(mapsStateToProps, mapDispatchToProps)(App);
